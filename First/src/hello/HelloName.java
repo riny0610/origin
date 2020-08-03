395d41f6dcd7c97646970816b4a6420e37ecdb8d
admin <admin@DESKTOP-EO18U0Q>
2020-08-03 11:12:28 +0900

    Initial commit

---
+++ First/src/hello/HelloName.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class HelloName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Çï·Î¿ì ¿ùµå");
+		System.out.println();
+	}
+
+}
---
+++ First/src/hello/Hellojava.java
@@ -0,0 +1,27 @@
+/*
+ * Date : 2020년 6월 26일
+ * AUtoer : 홍길동
+ * 설명 : 첫번째 자바 프로그램입니다.
+ * Version : 1.0
+ * 
+ */
+
+
+package hello;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		// 주석입니다. 코드를 설명 개발자가 보기 위함
+		 System.out.println("헬로우 자바");
+		 
+		 
+		 
+		 /*
+		  * 이것은
+		  * 여러줄
+		  * 주석
+		  */
+	}
+	
+}
